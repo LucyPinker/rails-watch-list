0b7c2f4072cdffe10507f0599087525d14c32145
lucypinker <lucypinker22@gmail.com>
2021-11-12 16:52:18 +0100

    progreSs

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,35 @@
+class ListsController < ApplicationController
+
+def index
+  @lists = List.all
+end
+
+def show
+  @list = List.find(params[:id])
+  @bookmarks = @list.bookmarks
+end
+
+def new
+  @list = List.new
+end
+
+def create
+@list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    end
+end
+
+def destroy
+   @list = List.find(params[:id])
+   if @list.destroy
+      redirect_to lists_path
+  end
+end
+
+private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+url = "http://tmdb.lewagon.com/movie/top_rated?api_key=rails-watch-list"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+
+movies["results"].each do |result|
+  Movie.create(
+    title: result['original_title'],
+    overview: result['overview'],
+    rating: result['vote_average'],
+    poster_url: result['poster_path']
+  )
+end
